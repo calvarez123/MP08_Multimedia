a4f8e382189650fd10ccb36b08679aec924da509
cristian <calvarezmartinez.cf@iesesteveterrades.com>
2023-12-11 16:58:37 +0100

    adsa

--- galeria/app/src/main/java/com/example/galeria/MainActivity.java
+++ galeria/app/src/main/java/com/example/galeria/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.galeria;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -13,48 +15,73 @@ import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+
     private ImageView imageView;
     private Uri imageUri;
+
+    private  ActivityResultLauncher<Intent> imagePickerLauncher;
+    private ActivityResultLauncher<Intent> cameraLauncher;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         imageView = findViewById(R.id.imageView);
 
-        // Configuración del ActivityResultLauncher
-        ActivityResultLauncher<String> imagePickerLauncher = registerForActivityResult(
-                new ActivityResultContracts.GetContent(),
-                new ActivityResultCallback<Uri>() {
+
+
+        imagePickerLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
                     @Override
-                    public void onActivityResult(Uri result) {
-                        if (result != null) {
-                            imageUri = result;
-                            try {
-                                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
-                                imageView.setImageBitmap(bitmap);
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            // There are no request codes
+                            Intent data = result.getData();
+                            Uri uri = data.getData();
+                            ImageView imageView = findViewById(R.id.imageView);
+                            imageView.setImageURI(uri);
                         }
                     }
                 });
 
-        // Botón para abrir la galería
         Button btnOpenGallery = findViewById(R.id.btnOpenGallery);
         btnOpenGallery.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                imagePickerLauncher.launch("image/*");
+                openGallery_result(v);
+            }
+        });
+
+        Button btnTakePhoto = findViewById(R.id.btnTakePhoto);
+        btnTakePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
             }
         });
     }
+
+    public void openGallery_result (View view){
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/*");
+        //Launch activity to get result
+        imagePickerLauncher.launch(intent);
+    }
+
+
+
 }
